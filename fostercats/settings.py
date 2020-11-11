1c5d198476de029a6f6cc60f589cbe3afd67fe6b
muhanpixie <22084931+muhanpixie@users.noreply.github.com>
2020-11-11 12:27:48 -0800

    cors header

--- fostercats/settings.py
+++ fostercats/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'apps.home.apps.HomeConfig',
     'apps.cats.apps.CatsConfig',
     'apps.customers.apps.CustomersConfig',
+    'corsheaders',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +120,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+if DEBUG == True:
+    CORS_ALLOW_ALL_ORIGINS = True
